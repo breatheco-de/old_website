fdd26a794daba778ad011ff2437ab1d32e6b945f
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-01-18 01:55:23 -0500

    textarea_raw_html

--- functions.php
+++ functions.php
@@ -153,6 +153,13 @@ function geeksAcademyIntegrationWithVisualComposter() {
       "category" => __( "Content", "code-preview"),
       "as_child" => array('only' => 'coderepl'),
       "params" => array(
+         array(
+            "type" => "checkbox",
+            "heading" => __( "Bootstrap active", "code-preview" ),
+            "param_name" => "bootstrap",
+            "value" => array('on'   => 'true' ),
+            "description" => __( "Import bootstrap into the iframe.", "code-preview" )
+         ),
         array(
             "type" => "textarea_raw_html",
             "holder" => "div",
@@ -230,15 +237,22 @@ add_shortcode( 'codehighliter', 'codehighliter_func' );
 
 function codepreview_func( $atts , $content = null) {
     extract( shortcode_atts( array(
+      'bootstrap' => 'false'
    ), $atts ) );
-   $content = wpb_js_remove_wpautop($content, true);
+   $content = wpb_js_remove_wpautop(base64_decode($content), true);
    $content = preg_replace( "/\r|\n/", "", $content );
-
+   
+   $contentBody = "<html><head>";
+   if($bootstrap and $bootstrap == 'true') $contentBody .= "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>";
+   $contentBody .= '</head><body>';
+   $contentBody .= $content;
+   $contentBody .= '</body></html>';
+   
    $frameId = 'codePreviewFrame'.rand(0,9999999);
    $htmlcontent = '<div class="code-preview"><iframe class="code-iframe" id="'.$frameId.'"  width="100%" frameBorder="0" src="about:blank"></iframe></div>
                   <script type="text/javascript">
                   var doc = document.getElementById(\''.$frameId.'\');
-                  doc.src = "data:text/html;charset=utf-8,'.$content.'";
+                  doc.src = "data:text/html;charset=utf-8,'.$contentBody.'";
                   </script>';
    return $htmlcontent;
 }
