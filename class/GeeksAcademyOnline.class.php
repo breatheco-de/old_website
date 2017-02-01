1bfde27518d754a301e1afe7772844f42b8db1c4
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-02-01 17:16:32 -0500

    mime type = zip

--- class/GeeksAcademyOnline.class.php
+++ class/GeeksAcademyOnline.class.php
@@ -3,7 +3,15 @@
 Class GeeksAcademyOnline {
 
 	function __construct() {
+		add_filter('upload_mimes', array($this,'custom_upload_mimes'));
 
+	}
+
+	function custom_upload_mimes ( $existing_mimes=array() ) {
+	    // add your extension to the mimes array as below
+	    $existing_mimes['zip'] = 'application/zip';
+	    $existing_mimes['gz'] = 'application/x-gzip';
+	    return $existing_mimes;
 	}
 
 	function createCourseHierarchy($menu_name) 
