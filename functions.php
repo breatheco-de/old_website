117ef0f2bb7a9a33cf6d348b29fd46c45c992c17
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-02-02 16:01:06 -0500

    blue skin

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ add_action( 'after_setup_theme', 'thedocs_child_theme_setup' );
 */
 function wmt_theme_style(){
  
+ wp_enqueue_style( 'style-skin', get_template_directory_uri().'/assets/css/skin-blue.css');
         
 }
 //add_action( 'wp_enqueue_scripts', 'wmt_theme_style' );
