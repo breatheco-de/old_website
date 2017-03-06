42eca54151c3ce8d91ea8d03ccd5358cc75f28f5
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-03-06 17:57:56 -0500

    css and html adjustments to assignment page

--- single-student-assignment.php
+++ single-student-assignment.php
@@ -7,32 +7,49 @@ if (have_posts()){
 
     $assignmentId = get_the_ID();
     $duedate = get_post_meta( $assignmentId, 'wpcf-assignment-due-date',true);
-    $thumbnail = get_the_post_thumbnail( $assignmentId, 'post_thumbnail', array( 'class' => 'alignleft' ) );
 
     $projectId = get_post_meta( $assignmentId, 'wpcf-project-assigned',true);
+    $thumbnail = get_the_post_thumbnail( $projectId, 'post_thumbnail', array( 'class' => 'alignleft img-responsive' ) );
     $projectContent = get_post_field('post_content', $projectId);
     $projectTitle = get_the_title($projectId);
     $filesUrl = get_post_meta( $projectId, 'wpcf-project-files',true);
     $duration = get_post_meta( $projectId, 'wpcf-project-hour-duration',true);
     $dificulty = get_post_meta( $projectId, 'wpcf-project-difficulty',true);
 ?>
-    <main class="container">
+    <main class="container main-content">
     <div class="row">
-        <!-- END Main content -->
-        <!-- Sidebar -->
-        <aside id="assignment-meta" class="col-md-5 col-sm-3">
-			<div class="">
-            	<h4>
-            		<i class="fa fa-calendar" aria-hidden="true"></i> Due date:
-            		<small><?php echo date('jS \of F: hA', $duedate); ?></small>
-            	</h4> 
+        <div class="col-sm-12">
+            <h2 class="single-title"><?php echo $projectTitle; ?></h2>
+        </div>
+    </div>
+    <div class="row">
+        <!-- Main content -->
+        <article class="col-md-8 col-sm-9" role="main">
+            <div class="row">
+                <div class="col-sm-12">
+                    <?php echo $projectContent; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    
+                </div>
+            </div>
+        </article>
+<!-- Sidebar -->
+        <aside class="col-md-4 col-sm-3">
+            <div class="">
+                <h4>
+                    <i class="fa fa-calendar" aria-hidden="true"></i> Due:
+                    <?php echo date('jS \of F', $duedate); ?>
+                </h4> 
             </div>
             <table class="table table-striped table-changelog">
               <tbody>
                 <tr>
                     <td>Technologies:</td>
                     <td>
-                        <?php $technologies = wp_get_post_terms($assignmentId,'project-technology');
+                        <?php $technologies = wp_get_post_terms($projectId,'project-technology');
                         foreach ($technologies as $technology) { ?>
                             <a href="<?php echo get_term_link($technology); ?>" class="label label-default"><?php echo $technology->name; ?></a>
                         <?php } ?>
@@ -52,7 +69,7 @@ if (have_posts()){
                 </tr>
               </tbody>
             </table>
-			<?php if($thumbnail and $thumbnail!='') { ?>
+            <?php if($thumbnail and $thumbnail!='') { ?>
                 <?php echo $thumbnail; ?>
             <?php } ?>
             <?php if($filesUrl and $filesUrl!=''){ ?>
@@ -64,25 +81,6 @@ if (have_posts()){
             </div>
             <?php } ?>
         </aside>
-        <!-- Main content -->
-        <article class="col-md-7 col-sm-9 main-content" role="main">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="single-title"><?php echo $projectTitle; ?></h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12">
-                    <?php echo $projectContent; ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12">
-                    
-                </div>
-            </div>
-        </article>
-
         <!-- END Sidebar -->
     </div>
 
