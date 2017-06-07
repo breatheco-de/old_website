34097d493e73fbe4c4275cb74d37d44588f4f6a6
Alejandro Sanchez <aalejo@gmail.com>
2017-06-07 20:53:31 +0000

    global assets constant

--- page-quiz.php
+++ page-quiz.php
@@ -4,7 +4,7 @@ get_header('boxed');
     <div class="container">
         <h1>Test your knowledge!</h1>
         <?php if(isset($_GET['qslug'])){ ?>
-            <iframe class="bcquiz" src="https://assets-alesanchezr.c9users.io/quiz/app/?slug=<?php echo $_GET['qslug']; ?>" width="100%" height="600" frameBorder="0">Browser not compatible.</iframe>
+            <iframe class="bcquiz" src="<?php echo ASSETS_URL; ?>quiz/app/?slug=<?php echo $_GET['qslug']; ?>" width="100%" height="600" frameBorder="0">Browser not compatible.</iframe>
         <?php } ?>
     </div>
 
