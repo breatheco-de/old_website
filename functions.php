ef42071468ae008cf6b8baf3963ee926291d240d
Alejandro Sanchez <aalejo@gmail.com>
2017-06-26 15:41:34 +0000

    refactoring of functions.php

--- functions.php
+++ functions.php
@@ -2,17 +2,32 @@
 
 include('class/utils/utils.autoload.php');
 
-include('class/WPSessionManagment.class.php');
-$WPSessionManagment = new WPSessionManagment();
+/**
+ * TODO: An attempt to have a single sessions in between several websites (wordpress or not)
+ * */
+//include('class/WPSessionManagment.class.php');
+//$WPSessionManagment = new WPSessionManagment();
 
 include('class/GeeksAcademyOnline.class.php');
 include('class/vc_composer/VisualComposerSettings.class.php');
 include('class/gravity_forms/GravityFormSettings.class.php');
 include('class/types/TypesSettings.class.php');
 try{
+    /**
+     * This class takes care of all the theme setup
+     * */
     $GeeksAcademyOnline = new GeeksAcademyOnline();
+    /**
+     * Everything related to the visual composer settings and components.
+     * */
     $VisualComposerSettings = new VisualComposerSettings();
+    /**
+     * Everything related to te gravityforms integration
+     * */
     $GravityFormOptions = new GravityFormSettings();
+    /**
+     * The custom post types like (Lesson, Lesson Assets, Course, Assignment, etc.).
+     * */
     $TypesSettings = new WPTypesSettings();
 }
 catch(\Exception $e)
@@ -20,8 +35,15 @@ catch(\Exception $e)
 	Utils\BCError::notifyError($e->getMessage());
 }
 
+/**
+ * This theme is multilangual and this class handles most of the logic by integrating with
+ * the PolilangPlugin.
+ * */
 include('class/WPLanguages.class.php');
 $WPLanguages = new WPLanguages();
 
+/**
+ * This class takes care of the adminisitation interface under appereache->theme_options
+ **/
 $BCThemeOptions = new BCThemeOptions();
 
