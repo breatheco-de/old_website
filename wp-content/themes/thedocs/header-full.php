6cca93ee89c3f65dd72916af016b302d52c09d87
Alejandro Sanchez <aalejo@gmail.com>
2017-08-10 01:39:33 +0000

    recovering plugins and themes

---
+++ wp-content/themes/thedocs/header-full.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<?php 
+$redux_demo = get_option('redux_demo'); ?>
+
+<html lang="en">
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <!-- Styles -->
+    <?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) {
+        ?>
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="<?php if(isset($redux_demo['favicon']['url'])){?><?php echo esc_url($redux_demo['favicon']['url']); ?><?php }?>" type="image/x-icon"/>
+    <?php }?>
+      <?php wp_head();?>
+      <!-- Favicons -->
+    
+</head>
+
+  <body <?php body_class(); ?>>
+
+    <header class="site-header">
+
+      <!-- Top navbar & branding -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+
+          <!-- Toggle buttons and brand -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="true" aria-controls="navbar">
+              <span class="glyphicon glyphicon-option-vertical"></span>
+            </button>
+
+            <button type="button" class="navbar-toggle for-sidebar" data-toggle="offcanvas">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <?php $redux_demo = get_option('redux_demo'); if(isset($redux_demo['logo']['url'])){?>
+                <?php  if($redux_demo['logo']['url'] != ''){ ?>
+                <img alt="<?php echo esc_html__( 'logo', 'thedocs' );?>" src="<?php echo esc_url($redux_demo['logo']['url']); ?>" class="logo-padding"/>
+                <?php }else{ ?>
+                <img  src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="<?php echo esc_html__( 'logo', 'thedocs' );?>" class="logo-padding" />
+                <?php }}else{?>
+                <img  src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="<?php echo esc_html__( 'logo', 'thedocs' );?>" class="logo-padding" />
+                <?php }?>
+            </a>
+          </div>
+          <!-- END Toggle buttons and brand -->
+
+          <!-- Top navbar -->
+          <div id="navbar" class="navbar-collapse collapse" aria-expanded="true" role="banner">
+            <?php 
+                      wp_nav_menu( 
+                      array( 
+                            'theme_location' => 'primary',
+                            'container' => '',
+                            'menu_class' => '', 
+                            'menu_id' => '',
+                            'menu'            => '',
+                            'container_class' => '',
+                            'container_id'    => '',
+                            'echo'            => true,
+                             'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                             'walker'            => new wp_bootstrap_navwalker(),
+                            'before'          => '',
+                            'after'           => '',
+                            'link_before'     => '',
+                            'link_after'      => '',
+                            'items_wrap'      => '<ul class="nav navbar-nav navbar-right  %2$s">%3$s</ul>',
+                            'depth'           => 0,        
+                        )
+                     ); ?>
+          </div>
+          <!-- END Top navbar -->
+
+        </div>
+      </nav>
+      <!-- END Top navbar & branding -->
+      
+    </header>
---
+++ wp-content/themes/thedocs/page-templates/blog.php
@@ -0,0 +1,84 @@
+ <?php /*
+ * Template Name:Blog
+ * Description: A Page Template with a Page Builder design.
+ */
+get_header();?> 
+
+ <main class="container">
+      <div class="row">
+
+        <!-- Main content -->
+        <div class="col-md-9 col-sm-9 main-content" role="main">
+                    <?php 
+                        $args = array(    
+                            'paged' => $paged,
+                            'post_type' => 'post',
+                            );
+                        $wp_query = new WP_Query($args);
+                        while ($wp_query -> have_posts()): $wp_query -> the_post(); 
+                        ?> 
+          <article class="post">
+            <header>
+              <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+              <ul class="post-meta">
+                <li><i>
+                <?php if(isset($redux_demo['blog_cat'])){?>
+                        <?php echo esc_attr($redux_demo['blog_cat']); ?>
+                        <?php }else{?>
+                        <?php echo esc_html__( 'Category:', 'thedocs' );
+                        }
+                        ?></i> <span><?php 
+                                            // Show all category for post
+                                            $i = 1; foreach((get_the_category()) as $category) { 
+                                            if ($i == 1){echo ''; }else {echo ' , ';}
+                                                echo '<a href="'.get_category_link($category->cat_ID).'"> '.$category->category_nicename . ' '.'</a>'; 
+                                                
+                                                $i++;
+                                            } ?></span></li>
+                <li><i><?php if(isset($redux_demo['blog_time'])){?>
+                        <?php echo esc_attr($redux_demo['blog_time']); ?>
+                        <?php }else{?>
+                        <?php echo esc_html__( 'Date:', 'thedocs' );
+                        }
+                        ?></i> <span><?php the_time('F j, Y'); ?></span></li>
+              </ul>
+            </header>
+            <?php if ( has_post_thumbnail() ) { ?>
+            <div class="post-media">
+              <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id());?>" alt="<?php the_title(); ?>">
+            </div>
+            <?php }?>
+            <div class="blog-content">
+              <p><?php echo thedocs_excerpt($redux_demo['blog_excerpt']); ?></p>
+            </div>
+            
+
+            <p class="read-more">
+              <a class="btn btn-teal btn-outline" href="<?php the_permalink();?>">
+              <?php if(isset($redux_demo['continue_reading'])){?>
+                        <?php echo esc_attr($redux_demo['continue_reading']); ?>
+                        <?php }else{?>
+                        <?php echo esc_html__( 'Continue reading', 'thedocs' );
+                        }
+                        ?></a>
+            </p>
+          </article>
+          <?php endwhile;?>
+
+          <ul class="pager">
+            <?php thedocs_pagination(); ?>
+          </ul>
+          
+        </div>
+        <!-- END Main content -->
+<!-- Sidebar -->
+        <aside class="col-md-3 col-sm-3 sidebar">
+
+           <?php get_sidebar();?>
+
+        </aside>
+        <!-- END Sidebar -->
+
+      </div>
+    </main>
+<?php get_footer();?>
