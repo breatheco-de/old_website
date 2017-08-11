f5b185ff5cae23ed736e590d465c6a53cec88bb2
Alejandro Sanchez <aalejo@gmail.com>
2017-08-11 21:58:53 +0000

    default page

--- wp-content/themes/thedocs-child/page.php
+++ wp-content/themes/thedocs-child/page.php
@@ -1,4 +1,12 @@
 <?php 
     
 global $post;
-get_template_part( VIEWS_PATH.'page/'.$post->post_name);
+if(!get_template_part( VIEWS_PATH.'page/'.$post->post_name)){ ?>
+    <?php get_header('boxed'); ?>
+    <div class="container-fluid">
+    <?php if(have_posts()){ the_post(); ?>
+            <?php the_content(); ?>
+    <?php } ?>
+    </div>
+    <?php get_footer(); ?>
+<?php } ?>
