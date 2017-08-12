7a4ad224144ccf004ffc7f69274bb57d53c07d28
Alejandro Sanchez <aalejo@gmail.com>
2017-08-12 06:28:42 +0000

    some style fixes

--- wp-content/themes/thedocs-child/single.php
+++ wp-content/themes/thedocs-child/single.php
@@ -1,3 +1,10 @@
 <?php 
 global $post; 
-get_template_part( VIEWS_PATH.'post/'.$post->post_type);
+if(!get_template_part( VIEWS_PATH.'post/'.$post->post_name)){ ?>
+    <div class="container-fluid">
+    <?php if(have_posts()){ the_post(); ?>
+            <?php the_content(); ?>
+    <?php } ?>
+    </div>
+    <?php wp_footer(); ?>
+<?php } ?>
