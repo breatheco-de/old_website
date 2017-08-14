6ab8a896aa36cbf4d5806741cacf279c66402aef
Alejandro Sanchez <a@4geeksacademy.com>
2017-08-14 18:05:32 -0500

    trying to fix login

--- wp-content/themes/thedocs-child/src/php/BreatheCode/Controller/Credentials.php
+++ wp-content/themes/thedocs-child/src/php/BreatheCode/Controller/Credentials.php
@@ -9,7 +9,7 @@ use WPAS\Exception\WPASException;
 class Credentials{
    
     function custom_login() {
-        header('Content-type: application/json');
+//        header('Content-type: application/json');
     	// first check if data is being sent and that it is the data we want
       	if ( isset( $_POST["username"] ) && isset( $_POST["password"] ) ) {
     		// now set our response var equal to that of the POST var (this will need to be sanitized based on what you're doing with with it)
@@ -28,9 +28,10 @@ class Credentials{
             else if (!$wpUser) WPASController::ajaxError('Error retrieving the wordpress user');
             		
             wp_clear_auth_cookie();
-    		wp_set_current_user( $wpUser->ID, $wpUser->user_login );
-            wp_set_auth_cookie  ( $wpUser->ID );
-        
+    		$result1 = wp_set_current_user( $wpUser->ID, $wpUser->user_login );
+            $result2 = wp_set_auth_cookie  ( $wpUser->ID, true );
+       		do_action( 'wp_login', $wpUser->user_login );
+			//print_r($result2); die(); 
 			WPASController::ajaxSuccess(get_permalink(get_page_by_path( 'my-courses' )));
     	}
     	
