48375a4f7e9945d78f57d103240d3ea3f975e82f
Alejandro Sanchez <aalejo@gmail.com>
2017-08-29 17:09:00 +0000

    Talent tree explanation

--- wp-content/themes/thedocs-child/src/php/view/_partials/modal-talent_tree_explanation.php
+++ wp-content/themes/thedocs-child/src/php/view/_partials/modal-talent_tree_explanation.php
@@ -1,12 +1,12 @@
 <div class="modal fade" id="modal-talent_tree_explanation" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class='container'>
-    	<h2>Win all badges and to become The Master Geek!</h2>
+    	<h2>Win all badges to become a Master Geek!</h2>
     	<img class="img-responsive" src="<?php echo get_stylesheet_directory_uri().'/assets/img/rigoberto-super.png'; ?>"></img>
 	    <p>
     	There are 4 ways to earn points: <br/>Taking Quizzes, Delivering Projects, Completing Challenges or Classroom Rewards.
 	    </p>
 	    <p>
-		You need to accumulate 100 points any particular badge in order to win it!
+		You need to earn -at least- 100 points in order to win one specific badge!
 	    </p>
 	    <a href="<?php echo get_permalink( get_page_by_path( 'profile' ) ); ?>" type="button" class="btn btn-lg btn-primary">Check your badges</a>
     </div>
