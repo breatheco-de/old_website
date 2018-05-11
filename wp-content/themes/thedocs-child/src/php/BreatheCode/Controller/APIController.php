efe74b053823568c9298add14fdd615a3b38e808
Alejandro Sanchez <aalejo@gmail.com>
2018-05-11 23:41:21 +0000

    prueba1

--- wp-content/themes/thedocs-child/src/php/BreatheCode/Controller/APIController.php
+++ wp-content/themes/thedocs-child/src/php/BreatheCode/Controller/APIController.php
@@ -9,9 +9,8 @@ class APIController{
     
     function getLessons(){
         global $wpdb;
-        
-        $rows = $wpdb->get_results("SELECT post_name, post_title, post_status, post_excerpt FROM dev_ac_posts WHERE post_type='lesson'");
-        
+        $rows = $wpdb->get_results("SELECT post_name, post_title, post_status, post_excerpt FROM wp_posts WHERE post_type='lesson'");
+        debug($rows);
         return $rows;
     }
     
