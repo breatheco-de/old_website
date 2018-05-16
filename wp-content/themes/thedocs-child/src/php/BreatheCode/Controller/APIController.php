fd99f212779a8957a6d21801581abc26d9325784
Alejandro Sanchez <aalejo@gmail.com>
2018-05-16 20:17:47 +0000

    filtering the lessons by status

--- wp-content/themes/thedocs-child/src/php/BreatheCode/Controller/APIController.php
+++ wp-content/themes/thedocs-child/src/php/BreatheCode/Controller/APIController.php
@@ -9,7 +9,11 @@ class APIController{
     
     function getLessons(){
         global $wpdb;
-        $rows = $wpdb->get_results("SELECT post_name, post_title, post_status, post_excerpt FROM {$wpdb->prefix}posts WHERE post_type='lesson'");
+        $status = '';
+        if(isset($_GET['status'])) $status = " and post_status='".sanitize_text_field($_GET['status'])."'";
+        
+        $query = "SELECT post_name, post_title, post_status, post_excerpt FROM {$wpdb->prefix}posts WHERE post_type='lesson'".$status;
+        $rows = $wpdb->get_results($query);
         return $rows;
     }
     
